53e391e5b386958af9d5169dc1ab8f591c53d07d
Andrew Cutts <acutts@acgeospatial.co.uk>
2022-05-04 18:53:28 +0100

    Update 02_tqdm.py

--- tips_tricks/02_tqdm.py
+++ tips_tricks/02_tqdm.py
@@ -3,8 +3,8 @@ import time
 
 # just wrap any iterable with tqdm(iterable)
 
-# for i in tqdm (range(100)):
-#     time.sleep(0.01)
+for i in tqdm (range(100)):
+    time.sleep(0.01)
 
 for i in tqdm (range(100), desc="Loading..."):
     # perhaps don't do this in real life!
